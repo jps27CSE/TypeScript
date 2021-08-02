06d0022a1304909f17ed6698b2024ef92ae4934a
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-08-02 21:05:11 +0600

    Explicit & Union Types

---
+++ output/Explicit & Union Types.js
@@ -0,0 +1,27 @@
+"use strict";
+var a;
+var b;
+a = 'jack pritom soren';
+b = 32;
+console.log(b, a);
+//array 
+var c = [];
+c.push('jack');
+console.log(c);
+var d = [];
+d.push('partho', 3);
+console.log(d);
+//object
+var e;
+e = {
+    name: 'jack',
+    number: 32
+};
+console.log(e);
+var f;
+f = {
+    name: 'nahian',
+    age: 43,
+    deccission: true
+};
+console.log(f);
---
+++ src/Explicit & Union Types.ts
@@ -0,0 +1,50 @@
+let a: string;
+
+let b: number;
+
+a = 'jack pritom soren'
+
+b = 32
+
+console.log(b, a)
+
+//array 
+
+let c: string[] = []
+
+c.push('jack')
+
+console.log(c)
+
+let d: (number | string)[] = [];
+
+d.push('partho', 3)
+
+console.log(d)
+
+
+//object
+
+let e: object
+
+e = {
+    name: 'jack',
+    number: 32
+}
+
+console.log(e)
+
+
+let f: {
+    name: string,
+    age: number,
+    deccission: boolean
+}
+
+f = {
+    name: 'nahian',
+    age: 43,
+    deccission: true
+}
+
+console.log(f)
