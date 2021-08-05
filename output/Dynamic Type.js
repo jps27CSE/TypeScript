9be3d04c53390e466679379bbdd109c9f3e92d39
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-08-06 01:38:26 +0600

    Dynamic Types

---
+++ output/Dynamic Type.js
@@ -0,0 +1,11 @@
+"use strict";
+var z = [];
+z.push(3);
+z.push('Bangladesh');
+var y;
+y = {
+    name: 'jack',
+    age: 21
+};
+console.log(z);
+console.log(y);
---
+++ src/Dynamic Type.ts
@@ -0,0 +1,18 @@
+let z: any[] =[]
+
+z.push(3) 
+z.push('Bangladesh')
+
+
+let y :{
+    name: any, 
+    age: any
+}
+
+y={
+    name: 'jack', 
+    age: 21
+}
+
+console.log(z)
+console.log(y)
