a423b1b0d57d4b9ac6649e0c82d726db9ff2434a
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-08-09 18:48:50 +0600

    use of functions

---
+++ output/useOf_Functions.js
@@ -0,0 +1,11 @@
+"use strict";
+var myFunction = function (a, b) {
+    console.log(a);
+    console.log(b);
+};
+myFunction('jack', 'pritom');
+var myFunctionTwo = function (a, c) {
+    console.log(a);
+    console.log(c);
+};
+myFunction('soren', 'partho');
---
+++ src/useOf_Functions.ts
@@ -0,0 +1,17 @@
+const myFunction = (a: string, b: string) => {
+    console.log(a)
+    console.log(b)
+}
+
+myFunction('jack', 'pritom')
+
+
+const myFunctionTwo = (a: number, c?: string) => {
+    console.log(a)
+    console.log(c)
+}
+
+myFunction('soren', 'partho')
+
+
+
