11e3ff0806d4caa4b75e26c4f7e76f67998e7867
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-08 13:06:18 +0600

    First Typescript program

---
+++ series/index.js
@@ -0,0 +1,6 @@
+var Username = 'jack pritom soren';
+var SumofNumber = function (number1, number2) {
+    console.log(number1 + number2);
+};
+console.log(Username);
+SumofNumber(2, 2);
---
+++ series/index.ts
@@ -0,0 +1,10 @@
+let Username = 'jack pritom soren'
+
+
+const SumofNumber = (number1:number,number2:number) => {
+    console.log(number1+number2)
+}
+
+
+console.log(Username)
+SumofNumber(2,2)
