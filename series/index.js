756591249a098a41d64a146e5e954bdcf60e8b66
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-09 13:36:44 +0600

    practice

--- series/index.js
+++ series/index.js
@@ -2,5 +2,21 @@ var Username = 'jack pritom soren';
 var SumofNumber = function (number1, number2) {
     console.log(number1 + number2);
 };
-console.log(Username);
+// console.log(Username)
 SumofNumber(2, 2);
+var nameOrValue = function (message) {
+    console.log(message);
+};
+nameOrValue('jack');
+nameOrValue(1);
+var name2;
+name2 = 'jack';
+name2 = 2;
+name2 = true;
+function voidFunction() {
+    console.log('hello world');
+}
+voidFunction();
+var nameObject;
+nameObject = { name: 'jack', id: 2 };
+nameObject.name = 'pritom';
--- series/index.ts
+++ series/index.ts
@@ -6,5 +6,32 @@ const SumofNumber = (number1:number,number2:number) => {
 }
 
 
-console.log(Username)
+// console.log(Username)
 SumofNumber(2,2)
+
+
+const nameOrValue = (message: number | string)=>{
+    console.log(message)
+}
+
+nameOrValue('jack')
+nameOrValue(1)
+
+
+let name2 : any;
+name2= 'jack'
+name2 = 2
+name2 = true 
+
+
+function voidFunction ():void {
+    console.log('hello world')
+}
+
+voidFunction()
+
+let nameObject : {name: string, id: number}
+
+nameObject= {name: 'jack', id:2}
+
+nameObject.name= 'pritom'
