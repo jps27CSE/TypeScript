af33238496315831347396b030d6ad45042dda2c
jps27CSE <jackpritomsoren@hotmail.com>
2024-01-12 20:04:46 +0600

    test

---
+++ series/custom-data-type.js
@@ -0,0 +1,6 @@
+var users;
+users = [];
+var user1;
+user1 = { userName: "anis", userId: 101 };
+users.push(user1);
+console.log(users);
---
+++ series/custom-data-type.ts
@@ -0,0 +1,9 @@
+let users: object[];
+users = [];
+
+let user1: { userName: string; userId: number };
+
+user1 = { userName: "anis", userId: 101 };
+users.push(user1);
+
+console.log(users);
